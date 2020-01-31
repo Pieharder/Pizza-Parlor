cdbff477dfb6502964ce154d1e8427613d8747dd
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-31 11:31:49 -0800

    Add all appropriate prototype methods.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,32 @@
-function Pizza(size,toppings) {
+function Pizza(size, toppings, price) {
   this.size = size;
   this.toppings = toppings;
+  this.price = price;
+}
+
+Pizza.prototype.size = function {
+  if (this.size === "small") {
+    this.price += 6;
+  } else if (this.size === "medium") {
+    this.price += 10;
+  } else
+    this.price += 16;
 }
 
 
+Pizza.prototype.pizzaToppings
+
+
+
+Pizza.prototype.price
+
+
+
+
+
+
+
+var customerPizza = new Pizza(0, [], 0)
 
 $(document).ready(function(){
   $("form#pizzaInput").submit(function(event){

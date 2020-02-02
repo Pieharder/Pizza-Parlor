ecc65d2dbf091484674cd101571639983f32b859
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-02 00:51:46 -0800

    Adjust display of price and ingredients.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ function Pizza(size, sauce, toppings) {
   this.toppings = toppings
 }
 
-
 Pizza.prototype.selectToppings = function (ingredients) {
   this.toppings.push(ingredients);
 
@@ -13,15 +12,16 @@ Pizza.prototype.selectToppings = function (ingredients) {
 
 Pizza.prototype.price = function() {
   var totalPrice = 0;
-  if (this.size === "small") {
+  if (this.size === "Small") {
     totalPrice += 6;
-  } else if (this.size === "medium") {
+  } else if (this.size === "Medium") {
     totalPrice += 10;
-  } else if (this.size === "large") {
+  } else if (this.size === "Large"){
     totalPrice += 16;
   }
   return totalPrice
 }
+
 // Front End Logic (User Sees)
 $(document).ready(function(){
   $("form#pizzaInput").submit(function(event){
@@ -30,22 +30,24 @@ $(document).ready(function(){
     var pizzaSauce = $("input:radio[name=pizzaSauce]:checked").val();
     var pizzaToppings = [];
     var customerPizza = new Pizza(pizzaSize, pizzaSauce, pizzaToppings);
-    $("input:checkbox[name=pizzaToppings]:checked").each(function() {
-      var selectedToppings = $(this).val();
-      customerPizza.selectToppings(selectedToppings);
+    $("input:checkbox[name=pizzaAdditions]:checked").each(function() {
+      var inputtedToppings = $(this).val();
+      customerPizza.selectToppings(inputtedToppings);
     });
  
     var finalSize = pizzaSize
     var finalSauce = pizzaSauce
     var finalToppings = pizzaToppings
-    var finalPrice = customerPizza.price()
-    
-    $("#finalPrice").html(finalPrice);
-    for (var i = 0; i < pizzaToppings.length; i += 1)
-      $("#finalToppings").append("<li id='finalToppings'>" + finalToppings[i] + "</li>")
+    var finalPrice = customerPizza.price();
+    console.log(finalToppings)
+    $("#finalPrice").html("$" + finalPrice);
+    for(var i = 0; i < pizzaToppings.length; i += 1);
+      $("#finalToppings").append("<li>" + finalToppings[i] + "</li>");
     $("#finalSauce").html(finalSauce);
     $("#finalSize").html(finalSize);
     $(".results").show();
     $("#pizzaInput").hide();
+    console.log(finalToppings)
+    console.log(finalPrice)
     });
   });
